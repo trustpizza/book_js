dbedcd86b0725a523395e4c8abd87519abe9b728
trustpizza <maxelolsson@gmail.com>
2023-01-13 17:26:24 -0600

    For loop now displays multiple features of books

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,7 @@ let myLibrary = ["To Kill a Mockingbird", "Slaughterhouse Five", "Davinci Code"]
 
 function Book() {
   // Constructor
+  // Books have an Author, a Title, A Page Count and a Read(t/f)
 }
 
 function addBookToLibrary() {
@@ -13,6 +14,11 @@ function showBooks() {
   // Loops through all books in library and prints their name
   for (let i = 0; i < myLibrary.length; i++) {
     let book = myLibrary[i]
-    console.log(book.name)
+
+    if (book.was_read) {
+      console.log(book.name + "by: " + book.author + ". This book has been read.")
+    } else {
+    console.log(book.name + "by: " + book.author + ". This book is unread.")
+    }
   }
 }
