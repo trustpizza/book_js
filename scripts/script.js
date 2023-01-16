0fb8089dd6f1a3c9d0ae450a285e8e20ebbd9a12
trustpizza <maxelolsson@gmail.com>
2023-01-16 17:09:44 -0600

    Restarting to clean up code

--- scripts/script.js
+++ scripts/script.js
@@ -19,6 +19,16 @@ function Book(author, title, pageCount, hasRead) {
   this.hasRead = hasRead
 };
 
+// Add change function to change status
+
+Book.prototype.changeReadStatus = function() {
+  if (this.hasRead === true) {
+    this.hasRead = false 
+  } else {
+    this.hasRead = true
+  }
+}
+
 // This down below is broken
 
 function updateLibrary() {
@@ -26,130 +36,3 @@ function updateLibrary() {
     myLibrary = JSON.parse(localStorage["myLibrary"]) // Update var myLibrary to include all local storage books
   }
 };
-
-
-function addBookToLibrary(book) {
-  // Add book to myLibrary
-
-  myLibrary.push(book) // Add book to myLib
-  setLocalStorageLibrary(myLibrary) // Set localstorage.myLib to all books
-  updateLibrary();
-};
-
-function setLocalStorageLibrary(library) {
-  localStorage.setItem("myLibrary", JSON.stringify(library));
-}
-
-function createBookDiv(book, index) {
-  let newDiv = document.createElement("div");
-  newDiv.classList.add("card");
-  newDiv.classList.add("card-" + index)
-
-  let title = document.createElement("div");
-  let author = document.createElement("div");
-  let pageCount = document.createElement("div");
-  let hasRead = document.createElement("div");
-
-  title.innerHTML = "Title: " + book["title"];
-  author.innerHTML = "Author: " + book["author"];
-  pageCount.innerHTML = "Page Count: " + book["pageCount"];
-  hasRead.innerHTML = "Read?: ";
-  addReadButton(hasRead, index);
-
-  newDiv.appendChild(title);
-  newDiv.appendChild(author);
-  newDiv.appendChild(pageCount);
-  newDiv.appendChild(hasRead);
-
-  libraryDiv.appendChild(newDiv); // Add new div to library
-}
-
-function addDeleteButton(bookDiv, index) {
-  let deleteButton = document.createElement("button")
-  deleteButton.innerText = "Delete Book"
-  
-  deleteButton.addEventListener("click", () => {
-    bookDiv.remove();
-
-    myLibrary.splice(index, index+1);
-    setLocalStorageLibrary(myLibrary);
-  })
-
-  bookDiv.appendChild(deleteButton);
-}
-
-function addReadButton(div, index) {
-  let readButton = document.createElement("button")
-  let book = myLibrary[index]
-  readButton.innerHTML = book["hasRead"]
-
-  readButton.addEventListener("click", () => {
-    if (book["hasRead"] === true) {
-      book["hasRead"] = false
-      myLibrary[index] = book
-
-      setLocalStorageLibrary(myLibrary)
-      location.reload()
-    } else {
-      book["hasRead"] = true
-      myLibrary[index] = book
-
-      setLocalStorageLibrary(myLibrary)
-      location.reload()
-    }
-  });
-
-  div.appendChild(readButton);
-}
-
-function showBooks() {
-  // Loops through all books in library and prints their name
-  updateLibrary();
-
-  for (let i = 0; i < myLibrary.length; i++) {
-    let book = myLibrary[i]
-
-    createBookDiv(book, i)
-  };
-
-  let library = libraryDiv.children
-
-  for (let i = 0; i < myLibrary.length; i++) {
-    addDeleteButton(library[i], i)
-  }
-
-};
-
-function resetBooks() {
-  updateLibrary();
-
-  libraryDiv.innerHTML = ""
-}
-
-// Buttons to work form
-showBooks()
-
-showBookButton.addEventListener("click", () => {
-  bookForm.classList.add("viewable");
-
-  hideBookButton = document.getElementById("hide-library");
-  hideBookButton.addEventListener("click", () => {
-    bookForm.classList.remove("viewable")
-  });
-
-  formSubmitButton = document.getElementById("submit");
-  formSubmitButton.addEventListener("click", () => {
-    event.preventDefault()
-
-    let author = document.getElementById("author");
-    let title = document.getElementById("title");
-    let pageCount = document.getElementById("pages");
-    let hasRead = document.getElementById("hasRead");
-
-    let book = new Book(author.value, title.value, pageCount.value, hasRead.checked)
-    addBookToLibrary(book)
-   
-    resetBooks() // Prevents duplicates
-    showBooks()
-  })
-});
